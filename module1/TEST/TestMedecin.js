9708d0b400e5bc086c0e2157fe9e0c1d64aea8f3
nicolas <nicolas.berrier360@gmail.com>
2026-09-21 15:35:59 +0200

    Import TEST

---
+++ module1/TEST/TestMedecin.js
@@ -0,0 +1,18 @@
+import {Adresse} from "../BO/Adresse.js";
+import {MedecinGeneraliste} from "../BO/MedecinGeneraliste.js";
+
+// Création d'un médecin
+const medecin = new MedecinGeneraliste("Dupont", "Jean", "0601020304");
+
+
+console.log("--------------- Médecin ---------------")
+medecin.afficher();
+console.log("--------------- Changement du numéro de ce médecin ---------------")
+medecin.numeroDeTelephone = "0706050403"
+console.log("Nouveau numéro de téléphone : " + medecin.numeroDeTelephone)
+console.log("------------------------------------------------------------------")
+
+// Changer le tarif
+console.log("--------------- Changement du tarif ---------------")
+MedecinGeneraliste.tarif = 30
+medecin.afficher();
